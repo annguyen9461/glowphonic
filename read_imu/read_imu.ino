432e59982f0acdb8530223831b88733098195b2c
An Nguyen <annguye946100@gmail.com>
2025-05-09 18:46:38 -0500

    Can read roll

--- read_imu/read_imu.ino
+++ read_imu/read_imu.ino
@@ -15,134 +15,134 @@ void setup(void) {
   while (!Serial)
     delay(10); // will pause Zero, Leonardo, etc until serial console opens
 
-  Serial.println("Adafruit ISM330DHCX test!");
+  // Serial.println("Adafruit ISM330DHCX test!");
 
   if (!ism330dhcx.begin_I2C()) {
     // if (!ism330dhcx.begin_SPI(LSM_CS)) {
     // if (!ism330dhcx.begin_SPI(LSM_CS, LSM_SCK, LSM_MISO, LSM_MOSI)) {
-    Serial.println("Failed to find ISM330DHCX chip");
+    // Serial.println("Failed to find ISM330DHCX chip");
     while (1) {
       delay(10);
     }
   }
 
-  Serial.println("ISM330DHCX Found!");
+  // Serial.println("ISM330DHCX Found!");
 
   // ism330dhcx.setAccelRange(LSM6DS_ACCEL_RANGE_2_G);
-  Serial.print("Accelerometer range set to: ");
-  switch (ism330dhcx.getAccelRange()) {
-  case LSM6DS_ACCEL_RANGE_2_G:
-    Serial.println("+-2G");
-    break;
-  case LSM6DS_ACCEL_RANGE_4_G:
-    Serial.println("+-4G");
-    break;
-  case LSM6DS_ACCEL_RANGE_8_G:
-    Serial.println("+-8G");
-    break;
-  case LSM6DS_ACCEL_RANGE_16_G:
-    Serial.println("+-16G");
-    break;
-  }
+  // Serial.print("Accelerometer range set to: ");
+  // switch (ism330dhcx.getAccelRange()) {
+  // case LSM6DS_ACCEL_RANGE_2_G:
+  //   Serial.println("+-2G");
+  //   break;
+  // case LSM6DS_ACCEL_RANGE_4_G:
+  //   Serial.println("+-4G");
+  //   break;
+  // case LSM6DS_ACCEL_RANGE_8_G:
+  //   Serial.println("+-8G");
+  //   break;
+  // case LSM6DS_ACCEL_RANGE_16_G:
+  //   Serial.println("+-16G");
+  //   break;
+  // }
 
   // ism330dhcx.setGyroRange(LSM6DS_GYRO_RANGE_250_DPS);
-  Serial.print("Gyro range set to: ");
-  switch (ism330dhcx.getGyroRange()) {
-  case LSM6DS_GYRO_RANGE_125_DPS:
-    Serial.println("125 degrees/s");
-    break;
-  case LSM6DS_GYRO_RANGE_250_DPS:
-    Serial.println("250 degrees/s");
-    break;
-  case LSM6DS_GYRO_RANGE_500_DPS:
-    Serial.println("500 degrees/s");
-    break;
-  case LSM6DS_GYRO_RANGE_1000_DPS:
-    Serial.println("1000 degrees/s");
-    break;
-  case LSM6DS_GYRO_RANGE_2000_DPS:
-    Serial.println("2000 degrees/s");
-    break;
-  case ISM330DHCX_GYRO_RANGE_4000_DPS:
-    Serial.println("4000 degrees/s");
-    break;
-  }
+  // Serial.print("Gyro range set to: ");
+  // switch (ism330dhcx.getGyroRange()) {
+  // case LSM6DS_GYRO_RANGE_125_DPS:
+  //   Serial.println("125 degrees/s");
+  //   break;
+  // case LSM6DS_GYRO_RANGE_250_DPS:
+  //   Serial.println("250 degrees/s");
+  //   break;
+  // case LSM6DS_GYRO_RANGE_500_DPS:
+  //   Serial.println("500 degrees/s");
+  //   break;
+  // case LSM6DS_GYRO_RANGE_1000_DPS:
+  //   Serial.println("1000 degrees/s");
+  //   break;
+  // case LSM6DS_GYRO_RANGE_2000_DPS:
+  //   Serial.println("2000 degrees/s");
+  //   break;
+  // case ISM330DHCX_GYRO_RANGE_4000_DPS:
+  //   Serial.println("4000 degrees/s");
+  //   break;
+  // }
 
   // ism330dhcx.setAccelDataRate(LSM6DS_RATE_12_5_HZ);
-  Serial.print("Accelerometer data rate set to: ");
-  switch (ism330dhcx.getAccelDataRate()) {
-  case LSM6DS_RATE_SHUTDOWN:
-    Serial.println("0 Hz");
-    break;
-  case LSM6DS_RATE_12_5_HZ:
-    Serial.println("12.5 Hz");
-    break;
-  case LSM6DS_RATE_26_HZ:
-    Serial.println("26 Hz");
-    break;
-  case LSM6DS_RATE_52_HZ:
-    Serial.println("52 Hz");
-    break;
-  case LSM6DS_RATE_104_HZ:
-    Serial.println("104 Hz");
-    break;
-  case LSM6DS_RATE_208_HZ:
-    Serial.println("208 Hz");
-    break;
-  case LSM6DS_RATE_416_HZ:
-    Serial.println("416 Hz");
-    break;
-  case LSM6DS_RATE_833_HZ:
-    Serial.println("833 Hz");
-    break;
-  case LSM6DS_RATE_1_66K_HZ:
-    Serial.println("1.66 KHz");
-    break;
-  case LSM6DS_RATE_3_33K_HZ:
-    Serial.println("3.33 KHz");
-    break;
-  case LSM6DS_RATE_6_66K_HZ:
-    Serial.println("6.66 KHz");
-    break;
-  }
+  // Serial.print("Accelerometer data rate set to: ");
+  // switch (ism330dhcx.getAccelDataRate()) {
+  // case LSM6DS_RATE_SHUTDOWN:
+  //   Serial.println("0 Hz");
+  //   break;
+  // case LSM6DS_RATE_12_5_HZ:
+  //   Serial.println("12.5 Hz");
+  //   break;
+  // case LSM6DS_RATE_26_HZ:
+  //   Serial.println("26 Hz");
+  //   break;
+  // case LSM6DS_RATE_52_HZ:
+  //   Serial.println("52 Hz");
+  //   break;
+  // case LSM6DS_RATE_104_HZ:
+  //   Serial.println("104 Hz");
+  //   break;
+  // case LSM6DS_RATE_208_HZ:
+  //   Serial.println("208 Hz");
+  //   break;
+  // case LSM6DS_RATE_416_HZ:
+  //   Serial.println("416 Hz");
+  //   break;
+  // case LSM6DS_RATE_833_HZ:
+  //   Serial.println("833 Hz");
+  //   break;
+  // case LSM6DS_RATE_1_66K_HZ:
+  //   Serial.println("1.66 KHz");
+  //   break;
+  // case LSM6DS_RATE_3_33K_HZ:
+  //   Serial.println("3.33 KHz");
+  //   break;
+  // case LSM6DS_RATE_6_66K_HZ:
+  //   Serial.println("6.66 KHz");
+  //   break;
+  // }
 
-  // ism330dhcx.setGyroDataRate(LSM6DS_RATE_12_5_HZ);
-  Serial.print("Gyro data rate set to: ");
-  switch (ism330dhcx.getGyroDataRate()) {
-  case LSM6DS_RATE_SHUTDOWN:
-    Serial.println("0 Hz");
-    break;
-  case LSM6DS_RATE_12_5_HZ:
-    Serial.println("12.5 Hz");
-    break;
-  case LSM6DS_RATE_26_HZ:
-    Serial.println("26 Hz");
-    break;
-  case LSM6DS_RATE_52_HZ:
-    Serial.println("52 Hz");
-    break;
-  case LSM6DS_RATE_104_HZ:
-    Serial.println("104 Hz");
-    break;
-  case LSM6DS_RATE_208_HZ:
-    Serial.println("208 Hz");
-    break;
-  case LSM6DS_RATE_416_HZ:
-    Serial.println("416 Hz");
-    break;
-  case LSM6DS_RATE_833_HZ:
-    Serial.println("833 Hz");
-    break;
-  case LSM6DS_RATE_1_66K_HZ:
-    Serial.println("1.66 KHz");
-    break;
-  case LSM6DS_RATE_3_33K_HZ:
-    Serial.println("3.33 KHz");
-    break;
-  case LSM6DS_RATE_6_66K_HZ:
-    Serial.println("6.66 KHz");
-    break;
-  }
+  // // ism330dhcx.setGyroDataRate(LSM6DS_RATE_12_5_HZ);
+  // Serial.print("Gyro data rate set to: ");
+  // switch (ism330dhcx.getGyroDataRate()) {
+  // case LSM6DS_RATE_SHUTDOWN:
+  //   Serial.println("0 Hz");
+  //   break;
+  // case LSM6DS_RATE_12_5_HZ:
+  //   Serial.println("12.5 Hz");
+  //   break;
+  // case LSM6DS_RATE_26_HZ:
+  //   Serial.println("26 Hz");
+  //   break;
+  // case LSM6DS_RATE_52_HZ:
+  //   Serial.println("52 Hz");
+  //   break;
+  // case LSM6DS_RATE_104_HZ:
+  //   Serial.println("104 Hz");
+  //   break;
+  // case LSM6DS_RATE_208_HZ:
+  //   Serial.println("208 Hz");
+  //   break;
+  // case LSM6DS_RATE_416_HZ:
+  //   Serial.println("416 Hz");
+  //   break;
+  // case LSM6DS_RATE_833_HZ:
+  //   Serial.println("833 Hz");
+  //   break;
+  // case LSM6DS_RATE_1_66K_HZ:
+  //   Serial.println("1.66 KHz");
+  //   break;
+  // case LSM6DS_RATE_3_33K_HZ:
+  //   Serial.println("3.33 KHz");
+  //   break;
+  // case LSM6DS_RATE_6_66K_HZ:
+  //   Serial.println("6.66 KHz");
+  //   break;
+  // }
 
   ism330dhcx.configInt1(false, false, true); // accelerometer DRDY on INT1
   ism330dhcx.configInt2(false, true, false); // gyro DRDY on INT2
@@ -160,23 +160,23 @@ void loop() {
   // Serial.println(" deg C");
 
   /* Display the results (acceleration is measured in m/s^2) */
-  Serial.print("\t\tAccel X: ");
-  Serial.print(accel.acceleration.x);
-  Serial.print(" \tY: ");
-  Serial.print(accel.acceleration.y);
-  Serial.print(" \tZ: ");
-  Serial.print(accel.acceleration.z);
-  Serial.println(" m/s^2 ");
+  // Serial.print("\t\tAccel X: ");
+  // Serial.print(accel.acceleration.x);
+  // Serial.print(" \tY: ");
+  // Serial.print(accel.acceleration.y);
+  // Serial.print(" \tZ: ");
+  // Serial.print(accel.acceleration.z);
+  // Serial.println(" m/s^2 ");
 
   /* Display the results (rotation is measured in rad/s) */
-  Serial.print("\t\tGyro X: ");
-  Serial.print(gyro.gyro.x);
-  Serial.print(" \tY: ");
-  Serial.print(gyro.gyro.y);
-  Serial.print(" \tZ: ");
-  Serial.print(gyro.gyro.z);
-  Serial.println(" radians/s ");
-  Serial.println();
+  // Serial.print("\t\tGyro X: ");
+  // Serial.print(gyro.gyro.x);
+  // Serial.print(" \tY: ");
+  // Serial.print(gyro.gyro.y);
+  // Serial.print(" \tZ: ");
+  // Serial.print(gyro.gyro.z);
+  // Serial.println(" radians/s ");
+  // Serial.println();
 
   // Tilt angles
   float ax = accel.acceleration.x;
@@ -186,27 +186,29 @@ void loop() {
   float roll = atan2(ay, az) * 180.0 / PI;
   float pitch = atan2(-ax, sqrt(ay * ay + az * az)) * 180.0 / PI;
 
-  Serial.print("Roll: ");
-  Serial.print(roll);
-  Serial.print(" deg, Pitch: ");
-  Serial.print(pitch);
-  Serial.println(" deg");
+  // Serial.print("Roll: ");
+  // Serial.print(roll);
+  // Serial.print(" deg, Pitch: ");
+  // Serial.print(pitch);
+  // Serial.println(" deg");
 
-  delay(100);
+  int mappedRoll = 0;
 
-  //  // serial plotter friendly format
+  // Map to 0–180 scheme
+  if (roll >= 0) {
+    mappedRoll = (int)roll;  // 0–90
+  } else {
+    mappedRoll = 91 + (int)(-roll - 1);  // -1 to -90 => 91 to 180
+  }
 
-  //  Serial.print(temp.temperature);
-  //  Serial.print(",");
+  // Pack into 2 bytes + delimiter
+  int b1 = (mappedRoll >> 3);
+  int b2 = (mappedRoll & 7);
 
-  //  Serial.print(accel.acceleration.x);
-  //  Serial.print(","); Serial.print(accel.acceleration.y);
-  //  Serial.print(","); Serial.print(accel.acceleration.z);
-  //  Serial.print(",");
+  Serial.write(b1);
+  Serial.write(b2);
+  Serial.write(255);
 
-  // Serial.print(gyro.gyro.x);
-  // Serial.print(","); Serial.print(gyro.gyro.y);
-  // Serial.print(","); Serial.print(gyro.gyro.z);
-  // Serial.println();
-  //  delayMicroseconds(10000);
+  delay(50);  // ~20 Hz
+
 }
